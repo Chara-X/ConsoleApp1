7f76ef2772facc5218b7ce06f83072b720401e52
Chara G <Chara G@DAISY>
2022-04-04 19:26:48 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        private static readonly Task Task = new(() => { });
+
+        private static readonly Action Action = () => { };
+        public static void Main(string[] args)
+        {
+            Action();
+            Person b = new Person();
+            Console.WriteLine("Hello, World!");
+            HttpMessageHandler;
+        }
+    }
+
+    public abstract class Person
+    {
+        public int Id { get; set; }
+
+        public virtual void Go()
+        {
+
+        }
+
+        public abstract void Do();
+    }
+}
+
